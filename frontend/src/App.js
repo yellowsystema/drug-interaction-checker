c357f2b28d8d99cee8f4f442f7358d0fafb22bd7
JudeRgamba <152448646+JudeRgamba@users.noreply.github.com>
2025-04-05 23:05:10 -0700

    added text boxes
    main content in app.js in front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,9 @@ import React, { useEffect, useState } from 'react';
 
 function App() {
   const [message, setMessage] = useState('');
+  const [illness, setIllness] = useState('');
+  const [medications, setMedications] = useState('');
+  const [allergies, setAllergies] = useState('');
 
   useEffect(() => {
     axios.get('http://localhost:5000/api/hello')
@@ -14,6 +17,38 @@ function App() {
     <div>
       <h1>React + Express REST API</h1>
       <p>Backend says: {message}</p>
+      
+      <div>
+        <label>
+          Illness:
+          <input 
+            type="text" 
+            value={illness} 
+            onChange={(e) => setIllness(e.target.value)} 
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Medications:
+          <input 
+            type="text" 
+            value={medications} 
+            onChange={(e) => setMedications(e.target.value)} 
+          />
+        </label>
+      </div>
+      
+      <div>
+        <label>
+          Allergies:
+          <input 
+            type="text" 
+            value={allergies} 
+            onChange={(e) => setAllergies(e.target.value)} 
+          />
+        </label>
+      </div>
     </div>
   );
 }
